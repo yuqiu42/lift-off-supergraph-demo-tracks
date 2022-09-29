ecb1f8499d36bb36bb0013dfb123db8404764485
Raph Terrier <r4ph@me.com>
2022-09-29 17:02:45 +0200

    server cleanup

--- src/index.js
+++ src/index.js
@@ -5,22 +5,21 @@ const typeDefs = gql(readFileSync(path.resolve(__dirname, "./schema.graphql"), {
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs, resolvers,
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI(),
-      };
-    },
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
+});
 
-  const { url, port } = await server.listen({ port: process.env.PORT || 4001 });
+server.listen({ port: process.env.PORT || 4001 }).then(({ url, port }) => {
   console.log(`
-      🚀  Server is running
-      🔉  Listening on port ${port}
-      📭  Query at ${url}
-    `);
-}
+    🚀  Server is running
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
+  `);
+});
 
-startApolloServer(typeDefs, resolvers);
